47153e838aed8952f6c80f1f49a754def52405a7
wpj1530882136 <wangpeijian@pku.edu.cn>
2016-09-23 13:25:22 +0800

    init project

---
+++ app/src/main/java/com/example/wangpeijian/miniweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wangpeijian.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Created by wangpeijian on 2016/9/21.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
